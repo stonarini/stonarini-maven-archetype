b45e364ca11a3e81b9ca6b7d27424e00220148d4
stonarini <stonarini@cifpfbmoll.eu>
2022-02-04 23:40:31 +0100

    add basic main method and basic test

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
